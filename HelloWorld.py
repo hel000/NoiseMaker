d150a9aa374a34502041db7787db939b3c546663
hel000 <matthijs_419@hotmail.com>
2016-11-30 14:08:22 +0100

    Hello World

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+        print("Hello World")
+
+
+if __name__ == '__main__':
+    unittest.main()
