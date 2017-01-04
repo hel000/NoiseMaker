0ada64d199c4d2c673f3f424e54fe45203cd3d67
Mty <mty@home>
2017-01-04 17:19:23 +0100

    First ready-for-implementation version of the scanner
    Doesn't support hidden AP's yet because holy fucking shit everything
    through the air is a mess. This will catch X amount of packages, filter
    by beacons and filter out the garbage before returning a dictionary full
    of the discovered AP's formatted "BSSID":"ESSID". Goodu lucku m8o.

--- ScapyScanTest.py
+++ ScapyScanTest.py
@@ -3,22 +3,39 @@
 
 import sys
 from scapy.all import *
+from dns.rdatatype import NULL
 
-intface = "wlan0mon"
-amount = 50 #amount of packages that will be listened to before 
-target = {}
-
-#sniff(iface=intface, prn=lambda x:x.sprintf("{Dot11Beacon:%Dot11.addr3%:%Dot11Beacon.info%}"), count=1) #Returns the following: FF:FF:FF:FF:FF:FF <ESSID>
-a = sniff(iface=intface, count=amount) # Stores all the captured information in a. a.addrx for dest and source
-# use a[9].payload.payload.info for the SSID
-
-for b in a:
-    if str(b.summary()).find("Dot11Beacon"):
-        print b.addr2
-        target[b.addr2] = b.payload.payload.info
-
-print "Choose from these targets:"
-print target
-for c, value in target:
-    print c + ":" + value
+def main():
+    intface = "wlan0mon"
+    amount = 50 #amount of packages that will be listened to before 
+    target = {}
     
+    #sniff(iface=intface, prn=lambda x:x.sprintf("{Dot11Beacon:%Dot11.addr3%:%Dot11Beacon.info%}"), count=1) #Returns the following: FF:FF:FF:FF:FF:FF <ESSID>
+    try:
+        print "Scanning..."
+        a = sniff(iface=intface, count=amount) # Stores all the captured information in a. a.addrx for dest and source
+    except socket.error, e:
+        print "Error, interface does not exist. " + str(e)
+        return 1
+    # use a[9].payload.payload.info for the SSID
+    
+    for b in a:
+        if str(b.summary()).find("Dot11Beacon") and not str(b.addr2) == "None": #Only process the current package if it is a beacon. Also, apparently find tends to just fail at its job.
+            #print b.addr2
+            try:
+                if str(b.payload.payload.info) is not "":
+                    target[b.addr2] = b.payload.payload.info
+                #else:
+                    #print "Possible hidden AP found. Functionality not supported."
+                    #target[b.addr2] = "<Hidden access point>"
+                    #print "Caught hidden AP(?). Details: " + str(b.summary())
+            except AttributeError, e: #Package was incorrect or no beacon. Removing item from dictionary.
+                print "Corrupt package caught. Skipping."
+    print "Choose from these targets:" 
+    print target
+    for c in target:
+        print c + ":" + target[c]
+    return target
+    
+if __name__ == '__main__':
+    print main()
